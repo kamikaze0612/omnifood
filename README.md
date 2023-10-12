56329ca7102871fe17a67a2f79e3a28178cb98a7
Buyantogtokh Bekhbayar <buynaa@Buyantogtokhs-MacBook-Pro.local>
2023-10-12 11:04:51 +0900

    Update v1.0.1
    Scrolling bugs fixed README.md added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Omnifood - AI powered online platform helps you eat healthy!
+
+Omnifood offers you variety of healthy menus that can help you stick to your routine depending on your diet. This project was made by referencing online tutorial in Udemy "Build Responsive Real World Websites with HTML5 and CSS3" by Jonas Schmedtmann.
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const headerNav = document.querySelector('.header__navigation');
-const headerMenuBtn = document.querySelector('.header__menu-btn');
-const allLinks = document.querySelectorAll('a:link');
-const heroSectionEl = document.querySelector('.section-hero');
+const headerNav = document.querySelector(".header__navigation");
+const headerMenuBtn = document.querySelector(".header__menu-btn");
+const allLinks = document.querySelectorAll("a:link");
+const heroSectionEl = document.querySelector(".section-hero");
 const body = document.body;
 
-headerMenuBtn.addEventListener('click', (event) => {
+headerMenuBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  body.classList.toggle('menu-active');
+  body.classList.toggle("menu-active");
 });
 
 if (window.innerWidth <= 768) {
-  headerNav.addEventListener('click', (event) => {
-    if (event.target.classList.contains('header__navigation')) {
-      body.classList.remove('menu-active');
+  headerNav.addEventListener("click", (event) => {
+    if (event.target.classList.contains("header__navigation")) {
+      body.classList.remove("menu-active");
     }
   });
 }
 
 allLinks.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    const href = link.getAttribute('href');
+  link.addEventListener("click", (event) => {
+    const href = link.getAttribute("href");
 
-    if (href === '#') {
+    if (href === "#") {
       event.preventDefault();
       window.scrollTo({
         top: 0,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     }
 
-    if (href !== '#' && href.startsWith('#')) {
+    if (href !== "#" && href.startsWith("#")) {
       event.preventDefault();
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
@@ -41,20 +41,20 @@ const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     console.log(ent);
-    ('');
+    ("");
 
     if (!ent.isIntersecting) {
-      body.classList.add('sticky');
+      body.classList.add("sticky");
     }
 
     if (ent.isIntersecting) {
-      body.classList.remove('sticky');
+      body.classList.remove("sticky");
     }
   },
   {
     root: null,
     threshold: 0,
-    rootMargin: '-96px',
+    rootMargin: "-96px",
   }
 );
 
